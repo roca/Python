036d2a0775e80378e0e5fa26128175b164ab7d25
ROMEL CAMPBELL <RomelCampbell@gmail.com>
2017-07-27 05:24:07 -0400

    WIP: Need get the class_vis to work

--- Udacity/ud120/ud120-projects/decision_tree/dt_author_id.py
+++ Udacity/ud120/ud120-projects/decision_tree/dt_author_id.py
@@ -25,7 +25,20 @@ features_train, features_test, labels_train, labels_test = preprocess()
 #########################################################
 ### your code goes here ###
 
+from sklearn.tree import tree
+clf = tree.DecisionTreeClassifier()
+t0 = time()
+clf.fit(features_train, labels_train)
+print "training time:", round(time()-t0, 3), "s"
 
+t1 = time()
+predicted = clf.predict(features_test) 
+print "prediction time:", round(time()-t1, 3), "s"
+
+from sklearn.metrics import accuracy_score
+accuracy = accuracy_score(predicted, labels_test)
+
+print(accuracy)
 #########################################################
 
 
