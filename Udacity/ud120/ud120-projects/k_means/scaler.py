c924247c56926926f2e48e23d29310cf845e615b
ROMEL CAMPBELL <RomelCampbell@gmail.com>
2017-09-04 06:18:20 -0400

    L10.11

--- Udacity/ud120/ud120-projects/k_means/scaler.py
+++ Udacity/ud120/ud120-projects/k_means/scaler.py
@@ -6,11 +6,11 @@ def featureScaling(arr):
     if len(arr) >= 3:
         try:
             scale = (float(arr[1]) - float(arr[0])) / (float(arr[2]) - float(arr[0]))
-        except divide-by-zero:
-            print "divide-by-zero"
+        except ZeroDivisionError:
+            print "ZeroDivisionError"
         
     return scale
 
 # tests of your feature scaler--line below is input data
-data = [115, 140, 175]
+data = [175, 140, 175]
 print featureScaling(data)
