2f15d9d1e62aa5d1dca6803daca702b079c1aa39
roca <romel.campbell@regeneron.com>
2019-06-13 11:40:19 -0400

    Copied code to colab

--- Udacity/ud120/titanic/fit.py
+++ Udacity/ud120/titanic/fit.py
@@ -1,4 +1,4 @@
-#!/usr/bin/pythonA
+#!/usr/bin/python
 
 # From: https://www.kaggle.com/sinakhorami/titanic-best-working-classifier
 
@@ -11,12 +11,19 @@ train = pd.read_csv('./train.csv', header = 0, dtype={'Age': np.float64})
 test  = pd.read_csv('./test.csv' , header = 0, dtype={'Age': np.float64})
 full_data = [train, test]
 
+line_break = "----------------------------------"
+
 print (train.info())
+print (line_break)
 
-print (train[['Pclass', 'Survived']].groupby(['Pclass'], as_index=False).mean()), "\n"
+print (train[['Pclass', 'Survived']].groupby(['Pclass'], as_index=False).mean())
+print (line_break)
 
-print (train[["Sex", "Survived"]].groupby(['Sex'], as_index=False).mean()), "\n"
+
+print (train[["Sex", "Survived"]].groupby(['Sex'], as_index=False).mean())
+print (line_break)
 
 for dataset in full_data:
     dataset['FamilySize'] = dataset['SibSp'] + dataset['Parch'] + 1
-print (train[['FamilySize', 'Survived']].groupby(['FamilySize'], as_index=False).mean()),"\n"
+print (train[['FamilySize', 'Survived']].groupby(['FamilySize'], as_index=False).mean())
+print (line_break)
