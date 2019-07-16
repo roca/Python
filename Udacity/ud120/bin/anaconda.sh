d2cd40594661246fb148622301ea57257944ed6b
roca <romel.campbell@regeneron.com>
2019-07-16 16:58:26 -0400

    Running Anaconda

---
+++ Udacity/ud120/bin/anaconda.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+#docker run -i -t -p 8888:8888 continuumio/miniconda /bin/bash -c "/opt/conda/bin/conda install jupyter -y --quiet && mkdir /opt/notebooks && /opt/conda/bin/jupyter notebook --notebook-dir=/opt/notebooks --ip='*' --port=8888 --no-browser"
+docker run -i -t -p 8888:8888 continuumio/anaconda /bin/bash -c "/opt/conda/bin/conda install jupyter -y --quiet && mkdir /opt/notebooks && /opt/conda/bin/jupyter notebook --notebook-dir=/opt/notebooks --ip='0.0.0.0' --port=8888 --no-browser --allow-root"
