6280b44a892740b8ab7cd6e0a8a8bfa550e47e71
ROMEL CAMPBELL <RomelCampbell@gmail.com>
2021-01-23 14:38:47 -0500

    Deployed new code GCP with env variables

--- udemy/google_cloud_functions_01/helloworld/main.py
+++ udemy/google_cloud_functions_01/helloworld/main.py
@@ -1,12 +1,29 @@
 from flask import abort
 import os
 
+def get_bearer_token(request):
+    bearer_token = request.headers.get('Authorization', None)
+    if not bearer_token:
+        abort(401)
+    parts = bearer_token.split()
+    if parts[0].lower() != "bearer":
+        # authorization header must start with 'Bearer'
+        abort(401)
+    elif len(parts) == 1:
+        # token was not found
+        abort(401)
+    elif len(parts) > 2:
+        # authorization header must be of the form 'Bearer token'
+        abort(401)
+    bearer_token = parts[1]
+    return bearer_token
+
 def hello_world(request):
-    
+
     if request.method != 'POST':
         abort(405)
 
-    bearer_token = request.headers.get('Authorization').split()[1]
+    bearer_token = get_bearer_token(request)
     secret_key = os.environ.get('ACCESS_TOKEN')
     if bearer_token != secret_key:
         abort(401)
