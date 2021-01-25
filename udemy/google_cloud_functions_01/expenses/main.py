286752239314b91b2b1cbf132c0ab21f9699b3da
ROMEL CAMPBELL <RomelCampbell@gmail.com>
2021-01-25 16:32:47 -0500

    New fucntion can insert records into firestore

--- udemy/google_cloud_functions_01/expenses/main.py
+++ udemy/google_cloud_functions_01/expenses/main.py
@@ -1,6 +1,21 @@
 import firebase_admin
 from firebase_admin import firestore
+from datetime import datetime
+import random
+
 firebase_admin.initialize_app()
 db = firestore.client()
 
+
+def set_expense(request):
+    try:
+        ref = db.collection('expenses').document()
+        ref.set({
+            'createdAt': datetime.now(),
+            'expenses': random.randint(1,200)
+        })
+    except Exception as e:
+        return f'Error: {e}', 400
+
+    return 'OK', 200
 
